b955b8e6e10b843e50cd4973bbf9766be1d41792
tufpsj <tufpsj@yonyou.com>
2022-01-21 11:23:27 +0800

    + initialize repo

---
+++ src/endpoints/index.ts
@@ -0,0 +1,32 @@
+import "../renderers";
+import { render as amisRender } from "amis";
+import ReactDOM from "react-dom";
+import { Schema } from "amis/lib/types";
+
+export type auditEndPointFunc = (
+  jsonSchema: Schema,
+  selector: string | HTMLElement
+) => JSX.Element;
+
+const auditEndPoint: auditEndPointFunc = (
+  jsonSchema: Schema,
+  selector: string | HTMLElement
+): JSX.Element => {
+  let comp = amisRender(jsonSchema);
+  if (typeof selector === "string") {
+    let selectorStr = selector;
+    selector = document.querySelector(selectorStr) as HTMLElement;
+    if (selector === null) {
+      throw new Error(`${selectorStr} not found`);
+    }
+  }
+  try {
+    ReactDOM.unmountComponentAtNode(selector as Element);
+  } catch (e) {}
+  ReactDOM.render(comp, selector);
+  return comp;
+};
+
+// bind to window
+(window as any).auditEndPoint = auditEndPoint;
+(window as any).embedAUI = auditEndPoint;
---
+++ src/example/App.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+
+const HEIGHT_THRESHOLD = 120;
+
+const bodyStyle = {
+  color: "#fafafa",
+  letterSpacing: 0,
+  textShadow:
+    "0px 1px 0px #999, 0px 2px 0px #888, 0px 3px 0px #777, 0px 4px 0px #666, 0px 5px 0px #555, 0px 6px 0px #444, 0px 7px 0px #333, 0px 8px 7px #001135",
+};
+
+const getWindowHeight = () => {
+  return window.innerHeight;
+};
+
+export default function App(props: any) {
+  const [mgtop, setMgtop] = useState((getWindowHeight() - HEIGHT_THRESHOLD) / 2);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setMgtop((getWindowHeight() - HEIGHT_THRESHOLD) / 2);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div style={bodyStyle}>
+      <h1 style={{ textAlign: "center", marginTop: mgtop, color: '#2c5d87', }}>
+        Hello, API Web UI library!
+      </h1>
+    </div>
+  );
+}
