c341b90e43fe504bed6370cd1536b3d6faeb3b92
tufpsj <tufpsj@yonyou.com>
2022-01-22 16:47:51 +0800

    # 提交周末工作内容

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -1,13 +1,89 @@
 import { render as amisRender } from "amis";
 import ReactDOM from "react-dom";
 import { Schema } from "amis/lib/types";
+import { fetcherConfig, RenderOptions } from "amis/lib/factory";
+import { RootRenderProps } from "amis/lib/Root";
+import axios from "axios";
 
-export type auditEndPointFunc = (
+export type embedFunc = (
   jsonSchema: Schema,
   selector: string | HTMLElement
 ) => JSX.Element;
 
-const auditEndPoint: auditEndPointFunc = (
+const defaultOptions: RenderOptions = {
+  // 下面三个接口必须实现
+  fetcher: ({
+    url, // 接口地址
+    method, // 请求方法 get、post、put、delete
+    data, // 请求数据
+    responseType,
+    config, // 其他配置
+    headers, // 请求头
+  }: any) => {
+    config = config || {};
+    config.withCredentials = true;
+    responseType && (config.responseType = responseType);
+
+    if (config.cancelExecutor) {
+      config.cancelToken = new (axios as any).CancelToken(
+        config.cancelExecutor
+      );
+    }
+
+    config.headers = headers || {};
+
+    if (method !== "post" && method !== "put" && method !== "patch") {
+      if (data) {
+        config.params = data;
+      }
+
+      return (axios as any)[method](url, config);
+    } else if (data && data instanceof FormData) {
+      config.headers = config.headers || {};
+      config.headers["Content-Type"] = "multipart/form-data";
+    } else if (
+      data &&
+      typeof data !== "string" &&
+      !(data instanceof Blob) &&
+      !(data instanceof ArrayBuffer)
+    ) {
+      data = JSON.stringify(data);
+      config.headers = config.headers || {};
+      config.headers["Content-Type"] = "application/json";
+    }
+
+    return (axios as any)[method](url, data, config);
+  },
+  isCancel() {
+    return false;
+  },
+  copy(contents: string) {
+    console.error("copy contents", contents);
+  },
+};
+
+/**
+export function render(
+  schema: Schema,
+  props: RootRenderProps = {},
+  options: RenderOptions = {},
+  pathPrefix: string = ''
+): JSX.Element
+ */
+
+const jsonSchemaRender: (
+  schema: Schema,
+  props?: RootRenderProps,
+  options?: RenderOptions,
+  pathPrefix?: string
+) => JSX.Element = (schema, props, options, pathPrefix) => {
+  if (!options) {
+    options = defaultOptions;
+  }
+  return amisRender(schema, props, options, pathPrefix);
+};
+
+const embed: embedFunc = (
   jsonSchema: Schema,
   selector: string | HTMLElement
 ): JSX.Element => {
@@ -26,10 +102,7 @@ const auditEndPoint: auditEndPointFunc = (
   return comp;
 };
 
-export {
-  auditEndPoint,
-}
+export { embed, jsonSchemaRender };
 
 // bind to window
-(window as any).auditEndPoint = auditEndPoint;
-(window as any).embedAUI = auditEndPoint;
+(window as any).embed = embed;
--- src/example/App.tsx
+++ src/example/App.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { auditEndPoint } from "../endpoints";
-import { Spinner } from "amis";
+import { jsonSchemaRender } from "../endpoints";
+// import { Spinner } from "amis";
 
-import 'amis/lib/themes/cxd.css';
-import 'amis/lib/helper.css';
-import 'amis/sdk/iconfont.css';
-import "./css/app.css";
+import "amis/lib/themes/cxd.css";
+import "amis/lib/helper.css";
+import "amis/sdk/iconfont.css";
+import "./css/app.scss";
 
 const HEIGHT_THRESHOLD = 200;
 
@@ -14,168 +14,179 @@ const getWindowHeight = () => {
 };
 
 const demoJsonSchema = {
-  "title": "浏览器内核对 CSS 的支持情况",
-  "remark": "嘿，不保证数据准确性",
-  "type": "page",
-  "body": {
-    "type": "crud",
-    "draggable": true,
-    "syncLocation": false,
-    "api": "/amis/api/mock2/sample",
-    "keepItemSelectionOnPageChange": true,
-    "autoGenerateFilter": true,
-    "bulkActions": [
+  title: "浏览器内核对 CSS 的支持情况",
+  remark: "嘿，不保证数据准确性",
+  type: "dialog",
+  actionType: "dialog",
+  size: "full",
+  // actions: [
+  //   {
+  //     type: "button",
+  //     actionType: "confirm",
+  //     label: "OK",
+  //     primary: true,
+  //   },
+  // ],
+  body: {
+    type: "crud",
+    draggable: true,
+    syncLocation: false,
+    api: "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/sample",
+    keepItemSelectionOnPageChange: true,
+    autoGenerateFilter: true,
+    bulkActions: [
       {
-        "type": "button",
-        "label": "批量删除",
-        "actionType": "ajax",
-        "api": "delete:/amis/api/mock2/sample/${ids|raw}",
-        "confirmText": "确定要批量删除?"
+        type: "button",
+        label: "批量删除",
+        actionType: "ajax",
+        api: "delete:https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/sample/${ids|raw}",
+        confirmText: "确定要批量删除?",
       },
       {
-        "type": "button",
-        "label": "批量修改",
-        "actionType": "dialog",
-        "dialog": {
-          "title": "批量编辑",
-          "name": "sample-bulk-edit",
-          "body": {
-            "type": "form",
-            "api": "/amis/api/mock2/sample/bulkUpdate2",
-            "body": [
+        type: "button",
+        label: "批量修改",
+        actionType: "dialog",
+        dialog: {
+          title: "批量编辑",
+          name: "sample-bulk-edit",
+          body: {
+            type: "form",
+            api: "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/sample/bulkUpdate2",
+            body: [
               {
-                "type": "hidden",
-                "name": "ids"
+                type: "hidden",
+                name: "ids",
               },
               {
-                "type": "input-text",
-                "name": "engine",
-                "label": "Engine"
-              }
-            ]
-          }
-        }
-      }
+                type: "input-text",
+                name: "engine",
+                label: "Engine",
+              },
+            ],
+          },
+        },
+      },
     ],
-    "quickSaveApi": "/amis/api/mock2/sample/bulkUpdate",
-    "quickSaveItemApi": "/amis/api/mock2/sample/$id",
-    "headerToolbar": [
+    quickSaveApi:
+      "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/sample/bulkUpdate",
+    quickSaveItemApi:
+      "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/sample/$id",
+    headerToolbar: [
       "bulkActions",
       {
-        "type": "button",
-        "label": "重置测试数据",
-        "actionType": "ajax",
-        "size": "sm",
-        "api": "/amis/api/mock2/sample/reset"
+        type: "button",
+        label: "重置测试数据",
+        actionType: "ajax",
+        size: "sm",
+        api: "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/sample/reset",
       },
       "export-excel",
       {
-        "type": "tpl",
-        "tpl": "一共有 ${count} 行数据。",
-        "className": "v-middle"
+        type: "tpl",
+        // eslint-disable-next-line no-template-curly-in-string
+        tpl: "一共有 ${count} 行数据。",
+        className: "v-middle",
       },
       {
-        "type": "columns-toggler",
-        "align": "right",
-        "draggable": true
+        type: "columns-toggler",
+        align: "right",
+        draggable: true,
       },
       {
-        "type": "drag-toggler",
-        "align": "right"
-      }
+        type: "drag-toggler",
+        align: "right",
+      },
     ],
-    "footerToolbar": [
-      "statistics",
-      "switch-per-page",
-      "pagination"
-    ],
-    "columns": [
+    footerToolbar: ["statistics", "switch-per-page", "pagination"],
+    columns: [
       {
-        "name": "id",
-        "label": "ID",
-        "width": 20,
-        "sortable": true,
-        "type": "text",
-        "searchable": {
-          "type": "input-text",
-          "name": "id",
-          "label": "主键",
-          "placeholder": "输入id"
-        }
-      },
-      {
-        "name": "browser",
-        "label": "Browser",
-        "searchable": {
-          "type": "select",
-          "name": "browser",
-          "label": "浏览器",
-          "placeholder": "选择浏览器",
-          "options": [
-            {
-              "label": "Internet Explorer ",
-              "value": "ie"
-            },
-            {
-              "label": "AOL browser",
-              "value": "aol"
-            },
-            {
-              "label": "Firefox",
-              "value": "firefox"
-            }
-          ]
-        }
-      },
-      {
-        "name": "platform",
-        "label": "平台",
-        "popOver": {
-          "trigger": "hover",
-          "body": {
-            "type": "tpl",
-            "tpl": "就是为了演示有个叫 popOver 的功能"
-          }
+        name: "id",
+        label: "ID",
+        width: 20,
+        sortable: true,
+        type: "text",
+        searchable: {
+          type: "input-text",
+          name: "id",
+          label: "主键",
+          placeholder: "输入id",
         },
-        "sortable": true,
-        "type": "text"
       },
       {
-        "name": "grade",
-        "label": "CSS 等级",
-        "type": "select",
-        "options": [
-          "A",
-          "B",
-          "C",
-          "D",
-          "X"
-        ]
+        name: "browser",
+        label: "Browser",
+        searchable: {
+          type: "select",
+          name: "browser",
+          label: "浏览器",
+          placeholder: "选择浏览器",
+          options: [
+            {
+              label: "Internet Explorer ",
+              value: "ie",
+            },
+            {
+              label: "AOL browser",
+              value: "aol",
+            },
+            {
+              label: "Firefox",
+              value: "firefox",
+            },
+          ],
+        },
       },
       {
-        "type": "operation",
-        "label": "操作",
-        "width": 100,
-        "buttons": [
+        name: "platform",
+        label: "平台",
+        popOver: {
+          trigger: "hover",
+          body: {
+            type: "tpl",
+            tpl: "就是为了演示有个叫 popOver 的功能",
+          },
+        },
+        sortable: true,
+        type: "text",
+      },
+      {
+        name: "grade",
+        label: "CSS 等级",
+        type: "select",
+        options: ["A", "B", "C", "D", "X"],
+      },
+      {
+        type: "operation",
+        label: "操作",
+        width: 100,
+        buttons: [
           {
-            "type": "button",
-            "actionType": "ajax",
-            "label": "删除",
-            "confirmText": "您确认要删除?",
-            "api": "delete:/amis/api/mock2/sample/$id"
-          }
-        ]
-      }
-    ]
-  }
+            type: "button",
+            actionType: "ajax",
+            label: "删除",
+            confirmText: "您确认要删除?",
+            api: "delete:https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/sample/$id",
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const buttonJsonScheme = {
+  type: "page",
+  body: {
+    label: "组件测试",
+    type: "button",
+    actionType: "dialog",
+    dialog: demoJsonSchema,
+  },
 };
 
 export default function App(props: any) {
   const [marginTop, setMarginTop] = useState(
     (getWindowHeight() - HEIGHT_THRESHOLD) / 2
   );
-
-  const [showJsonSchemaDemo, setShowJsonSchemaDemo] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -187,28 +198,12 @@ export default function App(props: any) {
     };
   }, []);
 
-  const handleClick = () => {
-    if (!showJsonSchemaDemo) {
-      setShowJsonSchemaDemo(true);
-      setTimeout(()=>{
-          auditEndPoint(demoJsonSchema, "#hello-json");
-      }, 2000);
-
-  } else {
-    setShowJsonSchemaDemo(false);
-  }
-  };
-
   return (
     <div>
       <div id="demo">
         <h1 style={{ marginTop: marginTop }}>Hello, AIP Web UI library!</h1>
       </div>
-      <div id="main-jump">
-        <span style={{cursor: 'pointer'}} onClick={handleClick}>
-          组件示例
-        </span>
-      </div> {showJsonSchemaDemo? <div id="hello-json"><div className="spinner"><Spinner></Spinner></div></div>: null}
+      <div id="main-jump">{jsonSchemaRender(buttonJsonScheme)}</div>{" "}
     </div>
   );
 }
